9f0669d50ba5975605d9a7430802a04184a41fe0
Steven DeVere <sdevere@xogrp.com>
2020-03-20 18:07:47 -0400

    Displays hexagonal pattern over map

--- components/Hexagon.jsx
+++ components/Hexagon.jsx
@@ -4,13 +4,39 @@ const hexagonStyle = {
   strokeWidth: 1
 }
 
-const Hexagon = () => (
-  <svg height="200" width="175">
-    <polygon
-      style={hexagonStyle}
-      points="87,0 173.603,50 173.603,150 87,200 0.397,150 0.397,50"
-    />
-  </svg>
-)
+const generateHexagonPoints = (height, width) => {
+  const x = [
+    width / 2,
+    width,
+    width,
+    width / 2,
+    0,
+    0
+  ]
+  const y = [
+    0,
+    height / 4,
+    height - (height / 4),
+    height,
+    height - (height / 4),
+    height / 4,
+  ]
+
+  return x
+    .reduce((str, val, idx) => str += `${val},${y[idx]} `, '')
+    .trim()
+}
+
+const Hexagon = ({ dimensions }) => {
+  const { height, width } = dimensions
+  return (
+    <svg height={`${height}px`} width={`${width}px`}>
+      <polygon
+        style={hexagonStyle}
+        points={generateHexagonPoints(height, width)}
+      />
+    </svg>
+  )
+}
 
 export default Hexagon
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,36 @@
 import Hexagon from '../components/Hexagon'
 
+const imgStyles = {
+  width: '800px',
+  height: '500px',
+  position: 'absolute'
+}
+
 const Index = () => {
-  const arr = new Array(10)
-  arr.fill(0)
+  const cellHeight = (500 / 26).toFixed(2)
+  const cellWidth = (800 / 44).toFixed(2)
+  const height = new Array(26).fill(0)
+  const width = new Array(44).fill(0)
+
+  const hexagonDimensions = {
+    width: cellWidth,
+    height: cellHeight,
+  }
+
   return (
     <div>
-      {arr.map((_, idx) => {
+      <img style={imgStyles} src={'/caucasus.png'} alt='caucasus' />
+      {height.map((_, idx) => {
         const rowStyle = {
           position: 'relative',
-          left: idx % 2 === 0 ? '0' : '87px',
-          bottom: `${53 * idx}px`
+          left: idx % 2 === 0 ? '0' : `${cellWidth/2}px`,
+          bottom: `${(cellHeight/4) * idx}px`,
+          height: `${cellHeight}px`,
         }
 
         return (
           <div style={rowStyle}>
-            <Hexagon />
-            <Hexagon />
-            <Hexagon />
+            {width.map((_, idx) => <Hexagon dimensions={hexagonDimensions} />)}
           </div>
         )
       })}
